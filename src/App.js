a76bf32a7f14470ca035f0202a0227e8285de56f
ssenick <ssenick@ukr.net>
2023-07-05 20:37:02 -0600

    fetch test + mocAPi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import {Header, Footer, PizzaBlock, Categories, Sort} from "./components";
 import './scss/app.scss'
+import {useEffect, useState} from "react";
 
 function App() {
+   const [isLoaded,setIsLoaded] = useState(false);
+   const [pizzas,setPizzas] = useState([]);
+   useEffect(() => {
+      fetch('https://64a6157600c3559aa9c054f6.mockapi.io/items')
+         .then(json => json.json())
+         .then(response => {
+            setPizzas(response)
+            setIsLoaded(true)
+         })
+   },[])
+
+   console.log(pizzas)
    return (
       <div className="App">
+
 
 
          <div className="wrapper">
@@ -14,9 +28,17 @@ function App() {
                      <Categories/>
                      <Sort/>
                   </div>
-                  <h2 className="content__title">Все пиццы</h2>
+                  <h2 className="content__title">All pizzas</h2>
                   <div className="content__items">
-                     <PizzaBlock/>
+                     {isLoaded
+                        ?
+                        pizzas.map((item,index) => (
+                           <PizzaBlock key={item.id} {...item}/>
+                        ))
+                        :
+                        <h1>LOading..</h1>
+                     }
+
                   </div>
                </div>
             </div>
--- src/components/PizzaBlock/index.js
+++ src/components/PizzaBlock/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const pizza = () => {
+const pizza = ({name,imageUrl,price}) => {
    return (
       <div className="pizza-block">
          <img
             className="pizza-block__image"
-            src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
+            src= {imageUrl}
             alt="Pizza"
          />
-         <h4 className="pizza-block__title">Чизбургер-пицца</h4>
+         <h4 className="pizza-block__title">{name}</h4>
          <div className="pizza-block__selector">
             <ul>
                <li className="active">тонкое</li>
@@ -21,7 +21,7 @@ const pizza = () => {
             </ul>
          </div>
          <div className="pizza-block__bottom">
-            <div className="pizza-block__price">from: 395 $</div>
+            <div className="pizza-block__price">from: {price} $</div>
             <div className="button button--outline button--add">
                <svg
                   width="12"
